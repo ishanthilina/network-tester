baf88c4e803aaeb720ef9a2c9fe59dc8f3d89b13
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2014-08-27 12:37:30 +0530

    output is only added to the log file. Program runs in the background using nohup

--- network-tester-client/run.sh
+++ network-tester-client/run.sh
@@ -2,10 +2,10 @@
 
 
 #Client paramemters
-URL="10.1.20.41"
+URL="72.251.248.20"
 PORT="6789"
 MAXDELAY="500"
 REQUESTINTERVAL="1000"
 TIMEOUT="1000"
 
-java -jar ./target/network-tester-client-1.0-SNAPSHOT.jar "$URL" "$PORT" "$MAXDELAY" "$REQUESTINTERVAL" "$TIMEOUT"
+nohup java -jar ./target/network-tester-client-1.0-SNAPSHOT.jar "$URL" "$PORT" "$MAXDELAY" "$REQUESTINTERVAL" "$TIMEOUT" &
--- network-tester-server/run.sh
+++ network-tester-server/run.sh
@@ -4,4 +4,4 @@
 PORT="6789"
 RESPONSEINTERVAL="1000"
 
-java -jar ./target/network-tester-server-1.0-SNAPSHOT.jar "$PORT" "$RESPONSEINTERVAL"
+nohup java -jar ./target/network-tester-server-1.0-SNAPSHOT.jar "$PORT" "$RESPONSEINTERVAL" &
